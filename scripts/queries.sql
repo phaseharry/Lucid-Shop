35640e2486f77c4510b3f100b41922bc52f13917
Harry C <harrychenhc@icloud.com>
2021-04-24 15:10:54 -0400

    add queries.sql
    Queries.sql stores all potential queries we have to make in the application. It will be used as a reference.

---
+++ scripts/queries.sql
@@ -0,0 +1,31 @@
+-- used for initial fetching of books with their authors
+SELECT B.title, B.img_url, B.unit_price, CONCAT(A.first_name, ' ', A.last_name) as author_name 
+FROM Book B
+JOIN Author A ON B.author_id = A.author_id;
+
+-- used to fetch all books written by a specific author
+SELECT B.title, B.img_url, B.unit_price, CONCAT(A.first_name, ' ', A.last_name) as author_name 
+FROM Author A 
+JOIN Book B ON B.author_id = A.author_id
+WHERE A.first_name = "Barack" AND A.last_name = "Obama";
+
+-- used for fetching a book based on title
+SELECT B.title, B.img_url, B.unit_price, CONCAT(A.first_name, ' ', A.last_name) as author_name 
+FROM Author A 
+JOIN Book B ON B.author_id = A.author_id
+WHERE B.title = "The Cat and the Hat";
+
+-- used for getting books of a certain category
+SELECT B.title, B.img_url, B.unit_price, CONCAT(A.first_name, ' ', A.last_name) as author_name 
+FROM Category C
+JOIN Book_to_Category BtC ON BtC.category_id = C.category_id 
+JOIN Book B ON BtC.bid = B.bid
+JOIN Author A ON A.author_id = B.author_id
+WHERE C.category_name = "Fantasy";
+
+-- used for getting books published by a certain publisher
+SELECT B.title, B.img_url, B.unit_price, CONCAT(A.first_name, ' ', A.last_name) as author_name 
+FROM Publisher P
+JOIN Book B ON P.publisher_id = B.publisher_id
+JOIN Author A ON A.author_id = B.author_id
+WHERE P.publisher_name = "Charles Scribner's Sons";
