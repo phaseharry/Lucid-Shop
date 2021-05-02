bb68cdb8e9f7f4da64ce2435e3462aae4c17b8c2
Harry <harrychenhc@icloud.com>
2021-05-01 22:50:44 -0400

    add frontend alerts to users when they add a book to cart

--- src/index.php
+++ src/index.php
@@ -11,6 +11,13 @@ include "includes/add_to_cart.inc.php";
 	<?php
 	include "navbar.php";
 	?>
+	<script>
+		function getCookie(name) {
+			const value = `; ${document.cookie}`;
+			const parts = value.split(`; ${name}=`);
+			if (parts.length === 2) return parts.pop().split(';').shift();
+		}
+	</script>
 </head>
 
 <body>
@@ -20,7 +27,7 @@ include "includes/add_to_cart.inc.php";
 		?>
 			<div class="book-item">
 				<div>
-					<h4><?php echo $b["title"] ?></h4>
+					<h4><?php echo $b["title"]; ?></h4>
 					<img src='<?php echo $b["img_url"]; ?>' alt='<?php $b["title"]; ?>' class="book-img">
 					<p><?php echo "Price:" . $b["unit_price"]; ?></p>
 					<p><?php echo "Author: " . $b["author_name"]; ?></p>
@@ -29,6 +36,16 @@ include "includes/add_to_cart.inc.php";
 					<input type="hidden" name="book_id" value="<?php echo $b["bid"]; ?>">
 					<button id='book_id_<?php echo $b["bid"]; ?>' type='submit' name="add-to-cart" class="btn btn-default">Add to cart</button>
 				</form>
+				<script>
+					document.getElementById("book_id_<?php echo $b["bid"]; ?>").addEventListener("click", () => {
+						const customer_id = getCookie("customer_id");
+						if (customer_id) {
+							alert(`"<?php echo $b["title"]; ?>" has been added to your cart`);
+						} else {
+							alert(`Please log in to add "<?php echo $b["title"]; ?>" to your cart`);
+						}
+					})
+				</script>
 			</div>
 		<?php
 		}
